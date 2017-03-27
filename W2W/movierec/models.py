f0ce66627018f83c90eaa5eaf560d9e5122db072
Quinn Zambeck <quinn95@gmail.com>
2017-03-27 15:45:36 -0500

    some models

--- W2W/movierec/models.py
+++ W2W/movierec/models.py
@@ -2,4 +2,33 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils.encoding import python_2_unicode_compatible
+
 # Create your models here.
+
+@python_2_unicode_compatible
+class Person(model.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.first_name
+
+@python_2_unicode_compatible
+class Genre(model.Model):
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Movie(model.Model):
+    title = models.CharField(max_length=30)
+    genre = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Person)
+    directors = models.ManyToManyField(Person)
+    date = models.DateField()
+    language = models.CharField(max_length=2) #may change
+
+    def __str__(self):
+        return self.title
