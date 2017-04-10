5cba035c69648fc7ea672725a6bae36bcb6e38d0
Quinn Zambeck <quinn95@gmail.com>
2017-04-10 14:40:57 -0500

    renamed

---
+++ W2W/apitesting/gb.py
@@ -0,0 +1,12 @@
+import guidebox
+import json
+
+#key : e9eb585ff0a9c36c22b6cf0fdc0a08cccfa5eac5
+guidebox.api_key = "e9eb585ff0a9c36c22b6cf0fdc0a08cccfa5eac5"
+movies = guidebox.Movie.list()
+
+
+
+
+
+
