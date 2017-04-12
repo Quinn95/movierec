941cc58f1e9929b9849c4fb1c209eb3da5fee13f
Raphael <raphaeldlst@utexas.edu>
2017-04-12 14:42:52 -0500

    carousel functional

---
+++ WhatToWatch/js/main.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+	var arrow = $('.arrow');
+	var tile = 211;
+	var animationSpeed = 450;
+	var carousel = new Array();
+	var numberOfTiles = new Array();
+
+	arrow.on('click', function(){
+		var side = $(this).attr('data-arrow');
+		var window_size = $(window).width() - 30;
+		var numTiles = window_size/tile | 0;
+		var id = side.split(" ")[1];
+
+		if(carousel[id] == undefined){
+			carousel[id] = 0;
+		}
+
+		if(numberOfTiles[id] == undefined){
+			numberOfTiles[id] = $(".c" + id).children(".tile").length;
+		}
+
+		if(side.split(" ")[0] == "left"){
+			carousel[id] = carousel[id] - numTiles * tile;
+			$(".c" + id).animate({scrollLeft: carousel[id]}, animationSpeed);
+			if(carousel[id] == 0){
+				$('.left' + id).hide();
+			}
+			if(carousel[id] < ((tile - 26) * numberOfTiles[id])){
+				$('.right' + id).fadeTo(10, 1);
+				$('.right' + id).removeAttr("disabled");
+			}
+		} 
+		else{
+			carousel[id] = carousel[id] + numTiles * tile;
+			if(carousel[id] != 0){
+				$('.left' + id).show();
+			}
+			// if(carousel[id] = )
+			$(".c" + id).animate({scrollLeft: carousel[id]}, animationSpeed);
+			if(carousel[id] >= ((tile - 26) * numberOfTiles[id])){
+				$('.right' + id).fadeTo(10, 0);
+				$('.right' + id).attr("disabled", "disabled");
+			}
+		}
+
+	});
+});
