954d3f1ccc0afcba1f56f47528d2f483b1311cde
Ali Ziyaan Momin <aliziyaan@wireless-10-145-59-189.public.utexas.edu>
2017-04-15 14:16:23 -0500

    updated data files

--- W2W/apitesting/gb.py
+++ W2W/apitesting/gb.py
@@ -1,17 +1,32 @@
 import guidebox
-import json
+# from tmdbv3api import TMDb
+#
+# tmdb = TMDb(api_key="f2eee9cde7536b5ef17767e4e9a97239", debug=False, lang="en")
+#
+# popular = tmdb.search("Tucker and Dale")
+#
+#
+# movie = tmdb.get_movie(343611)
+# print movie.title
+# print movie.overview
+# print movie.popularity
+# print movie.poster_path
+# print movie.amazon_link
+#
 
 #key : e9eb585ff0a9c36c22b6cf0fdc0a08cccfa5eac5
 guidebox.api_key = "e9eb585ff0a9c36c22b6cf0fdc0a08cccfa5eac5"
 guidebox.Region = "US"
 
-#movies = guidebox.Movie.list()
+movies = guidebox.Movie.list()
 
-source = guidebox.Movie.retrieve(id='5108')
-test = json.loads(source.__str__())
-for each in test['cast']:
-    print each['character_name'] + ': ' + each['name']
-#print movies
+# source = guidebox.Movie.retrieve(id='5108')
+print movies
+# test = json.loads(source.__str__())
+# for each in test['cast']:
+#     print each['character_name'] + ': ' + each['name']
+#
+# print movies
 
 
 
