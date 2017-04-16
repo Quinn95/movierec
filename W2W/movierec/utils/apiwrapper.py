db7a3fa7ca44271b13339508a62c349dfa9f4e88
Quinn Zambeck <quinn95@gmail.com>
2017-04-16 18:42:37 -0500

    more rec stuff

--- W2W/movierec/utils/apiwrapper.py
+++ W2W/movierec/utils/apiwrapper.py
@@ -8,12 +8,25 @@ def processRequest(request):
     #results = Movie.objects.filter(title__contains=request['title'])
     results = Movie.objects.all()
 
-    for stream in request['streaming_services']:
-        if stream == u'netflix':
-            results = results.exclude(netflix__isnull=True)
-        elif stream == u'amazon':
-            results = results.exclude(amazon__isnull=True)
-        elif stream == u'hulu':
-            results = results.exclude(hulu__isnull=True)
+    ###
+    #   Bug... Filters work as logical and, not logical or for streaming
+    #   services. Ie, if user checks netflix and amazon, it gets movies on
+    #   netflix && amazon.
+    ###
+    for k, v in request.items():
+        if k == 'streaming_services':
+            for stream in request[k]:
+                if stream == u'netflix':
+                    resultsnetflix = results.exclude(netflix__isnull=True)
+                elif stream == u'amazon':
+                    results = results.exclude(amazon__isnull=True)
+                elif stream == u'hulu':
+                    results = results.exclude(hulu__isnull=True)
+        elif k == 'title':
+            results = results.filter(title__contains=request[k])
+        elif k == 'people':
+            for person in results[k]:
+                results = result
+
 
     return results
