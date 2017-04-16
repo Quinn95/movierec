9f29494fffd31d8836dcd1709c491df71f6c74fc
Ali Ziyaan Momin <aliziyaan@Alis-MacBook-Pro.local>
2017-04-15 21:11:13 -0500

    messing around with querying the database

--- W2W/movierec/utils/gbox.py
+++ W2W/movierec/utils/gbox.py
@@ -13,23 +13,25 @@ m.save()
 """
 def testing():
     populateMovies(guidebox.Movie.list(limit=5))
+    populateMovies(guidebox.Movie.list(limit=5))
 
 def populateMovies(movies):
 
     list = json.loads(movies.__str__())
 
     for movie in list['results']:
-        m = Movie(identifier=movie['id'],
-                  themoviedb=movie['themoviedb'],
-                  imdb=movie['imdb'],
-                  rating=movie['rating'],
-                  rottentomatoes=movie['rottentomatoes'],
-                  wikipedia_id=movie['wikipedia_id'],
-                  metacritic=movie['metacritic'],
-                  common_sense_media=movie['common_sense_media'],
-                  poster=movie['poster_400x570'],
-                  title=movie['title'],
-                  date=movie['release_year'])
-        m.save()
-
+        e = Movie.objects.filter(identifier=movie['id'])
+        if e == None:
+            m = Movie(identifier=movie['id'],
+                      themoviedb=movie['themoviedb'],
+                      imdb=movie['imdb'],
+                      rating=movie['rating'],
+                      rottentomatoes=movie['rottentomatoes'],
+                      wikipedia_id=movie['wikipedia_id'],
+                      metacritic=movie['metacritic'],
+                      common_sense_media=movie['common_sense_media'],
+                      poster=movie['poster_400x570'],
+                      title=movie['title'],
+                      date=movie['release_year'])
+            m.save()
 
