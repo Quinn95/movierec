1c732619276850dafe2ff7c129cdb7eb160439f9
Quinn Zambeck <quinn95@gmail.com>
2017-04-16 18:05:40 -0500

    Created search formgit add views.py

--- W2W/movierec/utils/apiwrapper.py
+++ W2W/movierec/utils/apiwrapper.py
@@ -1,8 +1,19 @@
 import guidebox
 import gbox
-from movierec.models import Movie
+from ..models import *
 from django.db import models
 
 
+def processRequest(request):
+    #results = Movie.objects.filter(title__contains=request['title'])
+    results = Movie.objects.all()
 
+    for stream in request['streaming_services']:
+        if stream == u'netflix':
+            results = results.exclude(netflix__isnull=True)
+        elif stream == u'amazon':
+            results = results.exclude(amazon__isnull=True)
+        elif stream == u'hulu':
+            results = results.exclude(hulu__isnull=True)
 
+    return results
--- W2W/movierec/views.py
+++ W2W/movierec/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from utils import gbox
+from utils import apiwrapper
+
+from . import forms
 
 # Create your views here.
 
 def index(request):
     gbox.testing()
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def recView(request):
+    if request.method == 'POST':
+        form = forms.RecForm(request.POST)
+        if form.is_valid():
+            results = apiwrapper.processRequest(form.cleaned_data)
+            return render(request, 'movierec/getrec.html', {'results': results})
+    else:
+        form = forms.RecForm
+
+    return render(request, 'movierec/getrec.html', {'form': form})
 
