87e55a85d9ee944d6d06e46928a2e30cccdf83dc
Ali Ziyaan Momin <aliziyaan@wireless-10-146-162-9.public.utexas.edu>
2017-04-16 18:11:14 -0500

    added new fields to admin page

--- W2W/movierec/admin.py
+++ W2W/movierec/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Movie
+from .models import Movie, Genre, Person
 
 admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(Person)
 
