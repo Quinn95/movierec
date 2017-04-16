cfc4ebafc3980047821b23a2f65b26e22f2e14e0
Quinn Zambeck <quinn95@gmail.com>
2017-04-16 14:12:45 -0500

    added env

--- W2W/useraccount/views.py
+++ W2W/useraccount/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from account.views import SignupView as SignupViewBase
+
 # Create your views here.
+
+
+class SignupView(SignupViewBase):
+    def __init__(self):
+        super(self)
