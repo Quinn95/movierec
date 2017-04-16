a4bee3a9ecfd01b9bd60e8a8c1fac85197222719
Ali Ziyaan Momin <aliziyaan@wireless-10-146-162-9.public.utexas.edu>
2017-04-16 18:06:10 -0500

    added new fields into the database

--- W2W/movierec/models.py
+++ W2W/movierec/models.py
@@ -8,15 +8,16 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Person(models.Model):
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=15)
+    identifier = models.IntegerField(default=-1)
+    name = models.CharField(max_length=50, default='SOME STRING')
 
     def __str__(self):
-        return self.first_name
+        return self.name
 
 @python_2_unicode_compatible
 class Genre(models.Model):
-    name = models.CharField(max_length=15)
+    identifier = models.IntegerField(default=-1)
+    name = models.CharField(max_length=50, default='SOME STRING')
 
     def __str__(self):
         return self.name
@@ -33,9 +34,8 @@ class Movie(models.Model):
     common_sense_media = models.CharField(max_length=200, null=True)
     poster = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=30, null=True)
-    genre = models.ManyToManyField(Genre, null=True)
-    actors = models.ManyToManyField(Person, null=True)
-    #directors = models.ManyToManyField(Person)
+    genre = models.ManyToManyField(Genre)
+    people = models.ManyToManyField(Person)
     date = models.IntegerField(null=True)
     language = models.CharField(max_length=2, null=True) #may change
     netflix = models.CharField(max_length=200, null=True)
--- W2W/movierec/utils/gbox.py
+++ W2W/movierec/utils/gbox.py
@@ -1,6 +1,6 @@
 import guidebox
 import json
-from movierec.models import Movie
+from movierec.models import Movie, Genre, Person
 from django.db import models
 
 
@@ -14,7 +14,7 @@ m.themoviedb = "link"
 m.save()
 """
 def testing():
-    populateMovies(guidebox.Movie.list(offset=100, limit=100))
+    populateMovies(guidebox.Movie.list(limit=10))
     #populateMovies(guidebox.Movie.list(limit=100))
 
 def populateMovies(movies):
@@ -22,7 +22,6 @@ def populateMovies(movies):
     list = json.loads(movies.__str__())
 
     for movie in list['results']:
-
         e = Movie.objects.filter(identifier=int(movie['id']))
         if len(e) == 0:
             hulu_link = None
@@ -30,6 +29,25 @@ def populateMovies(movies):
             amazon_link = None
             movie_detail = guidebox.Movie.retrieve(movie['id'])
             detail = json.loads(movie_detail.__str__())
+            genre_list = []
+            for genre in detail['genres']:
+                if len(Genre.objects.filter(name=genre['title'])) == 0:
+                    g = Genre(identifier=genre['id'], name=genre['title'])
+                    genre_list.append(g)
+                    g.save()
+
+            person_list = []
+            for actor in detail['cast']:
+                if len(Person.objects.filter(name=actor['name'])) == 0:
+                    p = Person(identifier=actor['id'], name=actor['name'])
+                    person_list.append(p)
+                    p.save()
+            for director in detail['directors']:
+                if len(Person.objects.filter(name=director['name'])) == 0:
+                    d = Person(identifier=director['id'], name=director['name'])
+                    person_list.append(d)
+                    d.save()
+
             for each in detail['subscription_web_sources']:
                 if each['source'] == 'hulu_plus':
                     hulu_link = each['link']
@@ -52,7 +70,8 @@ def populateMovies(movies):
                       netflix=netflix_link,
                       amazon=amazon_link,
                       hulu=hulu_link)
-
-
+            m.save()
+            m.genre.add(*genre_list)
+            m.people.add(*person_list)
             m.save()
 
