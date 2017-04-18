59219494a46c4dd601b1c083ff970fc6414ee4eb
Quinn Zambeck <quinn95@gmail.com>
2017-04-17 19:29:18 -0500

    more apiwrapper

--- what2watch/movierec/utils/apiwrapper.py
+++ what2watch/movierec/utils/apiwrapper.py
@@ -7,9 +7,6 @@ from django.db import models
 def processRequest(request):
     #results = Movie.objects.filter(title__contains=request['title'])
     results = Movie.objects.all()
-    resultsn = Movie.objects.none()
-    resultsa = Movie.objects.none()
-    resultsh = Movie.objects.none()
     ###
     #   Bug... Filters work as logical and, not logical or for streaming
     #   services. Ie, if user checks netflix and amazon, it gets movies on
@@ -17,20 +14,26 @@ def processRequest(request):
     ###
     for k, v in request.items():
         if k == 'streaming_services':
-            for stream in v:
-                if stream == u'netflix':
-                    resultsn = results.exclude(netflix__isnull=True)
-                elif stream == u'amazon':
-                    resultsa = results.exclude(amazon__isnull=True)
-                elif stream == u'hulu':
-                    resultsh = results.exclude(hulu__isnull=True)
-            if len(v) != 0:
-                results = resultsn | resultsa | resultsh
-
+            results = filterStreamingServices(results, request[k])
         elif k == 'title':
             results = results.filter(title__contains=request[k])
         elif k == 'people':
             for person in request[k].split(','):
                 print person
-
     return results
+
+def filterStreamingServices(queryset, services):
+    resultsn = Movie.objects.none()
+    resultsa = Movie.objects.none()
+    resultsh = Movie.objects.none()
+    if len(services) != 0:
+        for stream in services:
+            if stream == u'netflix':
+                resultsn = queryset.exclude(netflix__isnull=True)
+            elif stream == u'amazon':
+                resultsa = queryset.exclude(amazon__isnull=True)
+            elif stream == u'hulu':
+                resultsh = queryset.exclude(hulu__isnull=True)
+        queryset = resultsn | resultsa | resultsh
+    return queryset
+
