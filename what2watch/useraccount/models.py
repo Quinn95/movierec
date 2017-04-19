0e5e65a19762ec60e83592e97c4daf6a6b5d8e10
Quinn Zambeck <quinn95@gmail.com>
2017-04-19 13:48:57 -0500

    more useraccount stuff. User can like/dislike movies

--- what2watch/useraccount/models.py
+++ what2watch/useraccount/models.py
@@ -2,4 +2,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+
+
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    liked_movies = models.ManyToManyField('movierec.Movie',
+                                          related_name='liked')
+    disliked_movies = models.ManyToManyField('movierec.Movie',
+                                             related_name='disliked')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
