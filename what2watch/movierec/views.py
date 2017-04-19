ea40b62bdf06b41912fed1097596353c33ccc672
Quinn Zambeck <quinn95@gmail.com>
2017-04-19 14:48:09 -0500

    Finished profiles. Working on actual website now

--- what2watch/movierec/views.py
+++ what2watch/movierec/views.py
@@ -8,18 +8,26 @@ from . import forms
 
 # Create your views here.
 
-def index(request):
+def home(request):
+    user = request.user
+    return render(request, 'movierec/home.html', {'user': user, 'movies' : None})
+
+def test(request):
     gbox.testing()
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("You got 10 movies")
 
 def recView(request):
     if request.method == 'POST':
         form = forms.RecForm(request.POST)
         if form.is_valid():
             results = apiwrapper.processRequest(form.cleaned_data)
-            return render(request, 'movierec/getrec.html', {'results': results})
+            return render(request, 'movierec/recpage.html',
+                          {'results': results,
+                           'form': form,
+                           'user': request.user})
     else:
         form = forms.RecForm
 
-    return render(request, 'movierec/getrec.html', {'form': form})
+    return render(request, 'movierec/getrec.html', {'form': form, 'user':
+                                                    request.user})
 
