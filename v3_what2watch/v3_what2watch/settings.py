4a704984921f2e9db3a63fdab6ff15fc1135c937
Quinn Zambeck <quinn95@gmail.com>
2017-04-22 16:25:25 -0500

    merged

--- v3_what2watch/v3_what2watch/settings.py
+++ v3_what2watch/v3_what2watch/settings.py
@@ -128,12 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
-=======
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
-
->>>>>>> 203f81c1b77258a13fe85dcc148152f3319b65fc
