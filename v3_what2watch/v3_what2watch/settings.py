017841474341b09f9d17d44389b1622d8a56312f
Quinn Zambeck <quinn95@gmail.com>
2017-04-22 16:22:10 -0500

    f

--- v3_what2watch/v3_what2watch/settings.py
+++ v3_what2watch/v3_what2watch/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'v3_what2watch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': os.path.join(BASE_DIR, 'dev.db'),
+        'NAME': 'dev', 
+        'USER': 'quinn',
+        'PASSWORD': '',
     }
 }
 
