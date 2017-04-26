89ec3bac0e357fbe14c6b8de4e903995dcf2da4f
Quinn Zambeck <quinn95@gmail.com>
2017-04-26 16:06:36 -0500

    merged tests

--- last_folder/v3_whattowatch/movierec/tests.py
+++ last_folder/v3_whattowatch/movierec/tests.py
@@ -58,6 +58,8 @@ class TestMovieModel(TestCase):
         result = Movie.objects.get(title="Test Movie")
         self.assertEquals(1, result.identifier)
 
+
+
 class TestRecommendationsForm(LiveServerTestCase):
     def setUp(self):
         #self.display = Display(visible=0, size=(1024, 768))
@@ -69,7 +71,7 @@ class TestRecommendationsForm(LiveServerTestCase):
         super(TestRecommendationsForm, self).setUp()
 
     def test_get_recommendations_action(self):
-        self.browser.get("http://localhost:8000/movierec/form/")
+        self.browser.get("http://immense-fortress-33702.herokuapp.com/movierec/form/")
         genre_input = self.browser.find_element_by_name('genre')
         for option in genre_input.find_elements_by_tag_name('option'):
             if option.text == "Action":
@@ -83,6 +85,96 @@ class TestRecommendationsForm(LiveServerTestCase):
         self.browser.implicitly_wait(20)
         #from_input = self.browser.find_element_by_name('from')
         #to_input = self.browser.find_element_by_name('to')
+
+        #this is a new test by Ali Momin
+    def test_get_language_english(self):
+        self.browser.get("http://immense-fortress-33702.herokuapp.com/movierec/form/")
+        lang_input = self.browser.find_element_by_name('language')
+        for lang in lang_input.find_elements_by_tag_name('option'):
+        	if lang.text == "English":
+        		lang.click()
+        		break
+        submit_input = self.browser.find_element_by_name('Submit')
+        submit_input.click()
+        modal = self.browser.find_element_by_name_id("myModal34688")
+        self.browser.implicitly_wait(20)
+
+    def test_get_netflix_movies(self):
+    	self.browser.get("http://immense-fortress-33702.herokuapp.com/movierec/form/")
+        stream_input = self.browser.find_element_by_name('netflix')
+        stream_input.click()
+        submit_input = self.browser.find_element_by_name('Submit')
+        submit_input.click()
+        modal = self.browser.find_element_by_name_id("myModal34688")
+        self.browser.implicitly_wait(20)
+
+    def test_get_amazon_movies(self):
+    	self.browser.get("http://immense-fortress-33702.herokuapp.com/movierec/form/")
+        stream_input = self.browser.find_element_by_name('amazon')
+        stream_input.click()
+        submit_input = self.browser.find_element_by_name('Submit')
+        submit_input.click()
+        modal = self.browser.find_element_by_name_id("myModal61261")
+        self.browser.implicitly_wait(20)
+
+    def test_get_hulu_movies(self):
+    	self.browser.get("http://immense-fortress-33702.herokuapp.com/movierec/form/")
+        stream_input = self.browser.find_element_by_name('hulu')
+        stream_input.click()
+        submit_input = self.browser.find_element_by_name('Submit')
+        submit_input.click()
+        modal = self.browser.find_element_by_name_id("myModal4627")
+        self.browser.implicitly_wait(20)
+
+    def test_year_range(self):
+        self.browser.get("http://immense-fortress-33702.herokuapp.com/movierec/form/")
+        from_input = self.browser.find_element_by_name('from')
+        for option in from_input.find_elements_by_tag_name('option'):
+            if option.text == "2008":
+                option.click()
+                break
+        to_input = self.browser.find_element_by_name('to')
+        for option in to_input.find_elements_by_tag_name('option'):
+            if option.text == "2008":
+                option.click()
+                break
+        submit_input = self.browser.find_element_by_name('Submit')
+        submit_input.click()
+        modal = self.browser.find_element_by_id("myModal34688")
+        self.browser.implicitly_wait(20)
+
+    def test_rating_r(self):
+        self.browser.get("http://localhost:8000/movierec/form/")
+        rating_input = self.browser.find_element_by_name('rating')
+        for option in rating_input.find_elements_by_tag_name('option'):
+            if option.text == "R":
+                option.click()
+                break
+        submit_input = self.browser.find_element_by_name('Submit')
+        submit_input.click()
+        modal = self.browser.find_element_by_id("myModal34688")
+        self.browser.implicitly_wait(20)
+
+    def test_year_amazon(self):
+        self.browser.get("http://localhost:8000/movierec/form/")
+        from_input = self.browser.find_element_by_name('from')
+        for option in from_input.find_elements_by_tag_name('option'):
+            if option.text == "2002":
+                option.click()
+                break
+        to_input = self.browser.find_element_by_name('to')
+        for option in to_input.find_elements_by_tag_name('option'):
+            if option.text == "2002":
+                option.click()
+                break
+        amazon_input = self.browser.find_element_by_name('amazon')
+        amazon_input.click()
+        submit_input = self.browser.find_element_by_name('Submit')
+        submit_input.click()
+        modal = self.browser.find_element_by_id("myModal14235")
+        self.browser.implicitly_wait(20)
+
+
 
 
     def tearDown(self):
