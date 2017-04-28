1f9c262b16c868926c31101523895bda53c29c50
Quinn Zambeck <quinn95@gmail.com>
2017-04-28 13:43:14 -0500

    fixed navbar

--- last_folder/v3_whattowatch/movierec/urls.py
+++ last_folder/v3_whattowatch/movierec/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views
 
 
+
 urlpatterns = [
-    url(r'^home/$', views.home, name="home"),
+    url(r'^$', views.home, name="home"),
     url(r'^form/$', views.recView, name="recommendations"),
     url(r'^search$', views.search, name='search'),
 ]
