c5ada57a882909140d5f80ffb662823a3330aec8
atchisonbrent <atchisonbrent@gmail.com>
2017-04-30 15:01:18 -0500

    forms.py

--- last_folder/v3_whattowatch/movierec/forms.py
+++ last_folder/v3_whattowatch/movierec/forms.py
@@ -1,15 +1,14 @@
 from django import forms
 
-STREAMING_CHOICES = (("netflix", "Netflix"), ("amazon", "Amazon"), ("hulu",
-                                                                    "Hulu"))
+STREAMING_CHOICES = (("netflix", "Netflix"), ("amazon", "Amazon"), ("hulu", "Hulu"))
 
 class RecForm(forms.Form):
-    people = forms.CharField(label="Peple", max_length=100, required=False)
-    title = forms.CharField(label="Title", max_length=100, required=False)
+    people = forms.CharField(label = "Peple", max_length = 100, required = False)
+    title = forms.CharField(label = "Title", max_length = 100, required = False)
     streaming_services = forms.MultipleChoiceField(
-        required=False,
-        widget=forms.CheckboxSelectMultiple,
-        choices=STREAMING_CHOICES
+        required = False,
+        widget = forms.CheckboxSelectMultiple,
+        choices = STREAMING_CHOICES
     )
 
 
