1e59d858e4dde6d75b3a13b071088ee6b349e292
atchisonbrent <atchisonbrent@gmail.com>
2017-04-30 15:07:03 -0500

    urls.py

--- last_folder/v3_whattowatch/movierec/urls.py
+++ last_folder/v3_whattowatch/movierec/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.home, name="home"),
-    url(r'^form/$', views.recView, name="recommendations"),
-    url(r'^search$', views.search, name='search'),
-    url(r'^like_dislike$', views.like_dislike, name="like_dislike"),
+    url(r'^$', views.home, name = "home"),
+    url(r'^form/$', views.recView, name = "recommendations"),
+    url(r'^search$', views.search, name = 'search'),
+    url(r'^like_dislike$', views.like_dislike, name = "like_dislike"),
 ]
